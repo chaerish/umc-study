2b637ad60bb78978b20620fe9239b4f26bc9ddc4
chaerish <ccyy8432@naver.com>
2024-10-29 16:29:25 +0900

    feat: set up

---
+++ study/src/main/java/com/umc/study/WorkbookApplication.java
@@ -0,0 +1,13 @@
+package com.umc.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbookApplication.class, args);
+	}
+
+}
---
+++ study/src/test/java/com/umc/study/WorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

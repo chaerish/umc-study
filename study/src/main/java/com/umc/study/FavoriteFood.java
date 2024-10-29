ec1681c85b9f20969eba3b8def06bacc09551db1
chaerish <ccyy8432@naver.com>
2024-10-29 17:15:07 +0900

    feat(Food): Food 엔티티 생성

---
+++ study/src/main/java/com/umc/study/FavoriteFood.java
@@ -0,0 +1,21 @@
+package com.umc.study;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+@Entity
+public class FavoriteFood {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long favoriteFoodId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+}
